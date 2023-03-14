6448221543484b87e37a13c2d1292e5285512aaf
Jordan <jordanboardman15@gmail.com>
2023-03-13 22:40:19 -0400

    save3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import BaseLayout from "./components/layout/BaseLayout.js";
-import App from "./components/App";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
 import Error from "./components/Error";
