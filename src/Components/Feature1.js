d33fb237665c154d22212d9474bd0400b436ea71
Jordan <jordanboardman15@gmail.com>
2023-03-27 11:09:42 -0400

    upd feature1 with axios get call for randomuser api

--- src/Components/Feature1.js
+++ src/Components/Feature1.js
@@ -1,36 +1,86 @@
 import React from "react";
-import { Paper, Box, Typography } from "@mui/material";
+import { Paper, Box, Typography, Avatar } from "@mui/material";
+import axios from "axios";
 
 function Feature1() {
+  const [post, setPost] = React.useState({
+    users: [],
+    isLoading: true,
+    errors: null,
+  });
+
+  React.useEffect(() => {
+    axios
+      .get("https://randomuser.me/api/?results")
+      .then((response) =>
+        response.data.results.map((user) => ({
+          name: `${user.name.first} ${user.name.last}`,
+          username: `${user.login.username}`,
+          email: `${user.email}`,
+          image: `${user.picture.large}`,
+        }))
+      )
+      .then((users) => {
+        setPost({
+          users,
+          isLoading: false,
+        });
+      })
+      .catch((error) => setPost({ error, isLoading: false }));
+  }, []);
+
+  if (!post) return null;
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-evenly",
-        flexWrap: "wrap",
-        "& > :not(style)": {
-          m: "8px",
-          width: "32vw",
-          height: "80vh",
-          marginTop: "58px",
-          backgroundColor: "#B99B6B",
-        },
-      }}
-    >
-      <Paper elevation={3}>
-        <Typography display="flex" justifyContent="center">
-          Avatar here
-        </Typography>
-        <br />
-        <Typography>Username</Typography>
-        <br />
-        <Box>
-          <Paper sx={{ backgroundColor: "#F1DBBF" }}>hello</Paper>
-        </Box>
-      </Paper>
-      <Paper elevation={3} />
-      <Paper elevation={3} />
-    </Box>
+    <React.Fragment>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+          "& > :not(style)": {
+            m: "8px",
+            width: "32vw",
+            height: "80vh",
+            marginTop: "58px",
+            backgroundColor: "#B99B6B",
+          },
+        }}
+      >
+        <Paper elevation={3}>
+          {!post.isLoading ? (
+            post.users.map((user) => {
+              const { username, name, email, image } = user;
+              return (
+                <Box key={username}>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Typography>{name}</Typography>
+                  </Box>
+
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Avatar
+                      sx={{
+                        width: 150,
+                        height: 150,
+                      }}
+                      src={image}
+                      alt={name}
+                    />
+                  </Box>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Typography>{username}</Typography>
+                  </Box>
+                </Box>
+              );
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
+        </Paper>
+        <Paper elevation={3} />
+        <Paper elevation={3} />
+      </Box>
+    </React.Fragment>
   );
 }
 
