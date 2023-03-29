604a7f2e89c3fcfc31e77729cc1efbdad7a349b3
Esther75 <esthermorris775@gmail.com>
2023-03-29 10:14:59 -0400

    user login reroutes to user page and shows dynamic user data

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,30 +3,35 @@ import React, { useState, setError } from "react";
 // import TextField from "@mui/material/TextField";
 // import { Button } from "@mui/material";
 // import { useNavigate } from "react-router-dom";
+import UserShow from "./UserShow";
 import axios from "axios";
 
 export function Login () {
 const [errors, setErrors] = useState([]);
 
 const handleSumbit = (event) => {
+  console.log("in handleSubmit");
   event.preventDefault();
   setErrors([]);
   const params = new FormData(event.target);
   axios
   .post("http://localhost:3000/sessions.json", params)
   .then((response)=> {
-    console.log(response.data);
     axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
     localStorage.setItem("jwt", response.data.jwt);
+    localStorage.setItem("user_id", response.data.user_id);
+    const user_id = window.localStorage.getItem(`user_id`)
     event.target.reset();
-    // event.preventDefault();
-    window.location.href = "/" //change this to hide a model, redirect to a specific page etc.
+    event.preventDefault();
+    window.location.href = "/user" //change this to hide a model, redirect to a specific page etc.
   })
   .catch ((error)=>{
     console.log(error.reponse)
     setError(["Invalid Email or Password"]);
   });
 };
+
+
 
 return (
   <div id="login">
@@ -44,7 +49,13 @@ return (
           Password: <input name="password" type="password"/>
         </div>
         <button type="submit">Login</button>
-      </form>
+    </form>
+ <br/>
+ <br/>
+ 
+ {/* user_id currently logged in: {user} */}
+      {/* <UserShow user={user}/> */}
+      #need to padd user to UserShow
   </div>
   );
 }
