1c272d462103db4290378264bdd13e8855ae4fc4
Jordan <jordanboardman15@gmail.com>
2023-04-24 16:56:49 -0400

    built Feature2Card.js - about to test

---
+++ src/Components/Feature2Card.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+function Feature2Card() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader title="{name}" />
+      <CardMedia
+        component="img"
+        height="194"
+        image=""
+        alt="ripple business picture"
+      />
+      <CardContent>
+        <Typography variant="h6" color="text.secondary">
+          short mission statement for impact partner?
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        {/* Here's where the GET request fuction will be called */}
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Descripton of Impact Partner</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
+
+export default Feature2Card;
