13b68b77c862983e81c8bc0c6da28b19e4c0dbab
Jordan <jordanboardman15@gmail.com>
2023-04-24 16:37:45 -0400

    added rfce to each page

---
+++ src/pages/EduPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EduPage() {
+  return <div>EduPage</div>;
+}
+
+export default EduPage;
---
+++ src/pages/WorldHunPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WorldHunPage() {
+  return <div>WorldHunPage</div>;
+}
+
+export default WorldHunPage;
