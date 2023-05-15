ecf7770eba69c9844c6d60c57c0e6e051adfa32f
Jordan <jordanboardman15@gmail.com>
2023-05-15 16:00:24 -0400

    updated Login.js component

--- src/index.js
+++ src/index.js
@@ -21,6 +21,8 @@ import WildPage from "./pages/WildPage";
 import SelectCatPage from "./pages/SelectCatPage";
 import "./index.css";
 import UpdateUserProfile from "./components/UpdateUserProfile";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
