fd89b2a1c94aad15eaf8afa23c607b5a027df7c2
Esther75 <esthermorris775@gmail.com>
2023-06-09 11:49:09 -0400

    dynamic individual catagories page!

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,10 +1,49 @@
 import React from "react";
 import Feature2Card from "../components/Feature2Card";
 import { Box, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Login } from "@mui/icons-material";
+
 
 function Category() {
+
+const [nonprofits, setNonprofits] = useState([])
+const urlSegment = window.location.href.split('/').pop()
+
+
+useEffect(() => {
+  getNonPorfits();
+}, []); // Empty dependency array ensures it only runs once on mount
+
+const getNonPorfits = () => {
+  console.log("getting Catagores");
+  fetch("http://localhost:3000/nonprofits.json")
+  .then (response => response.json())
+  .then(data => {
+      const filterNonprofits = data.message.filter(nonprofit => nonprofit.catagory_id === parseInt(urlSegment));
+      setNonprofits(filterNonprofits);
+    })
+    .catch(error => {
+      console.error('Error fetching nonprofits:', error);
+    });
+};
+
+
+
   return (
     <>
+
+    <div>Nonprofits
+      <ul>
+        {nonprofits.map(nonprofit => (
+          <li key={nonprofit.id}>{nonprofit.name}</li>
+        ))}
+      </ul>
+
+      <b> {urlSegment}</b>
+    </div>
+
       <Box
         sx={{
           display: "flex",
@@ -22,7 +61,7 @@ function Category() {
           }}
           variant="h2"
         >
-          Education
+          Catagory
         </Typography>
       </Box>
       <Box
@@ -35,25 +74,14 @@ function Category() {
           borderRadius: "20px",
         }}
       >
-        <Feature2Card />
-        <Feature2Card />
-        <Feature2Card />
-        <Feature2Card />
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-evenly",
-          padding: "20px",
-          margin: "20px",
-          backgroundColor: "#B99B6B",
-          borderRadius: "20px",
-        }}
-      >
-        <Feature2Card />
-        <Feature2Card />
-        <Feature2Card />
-        <Feature2Card />
+        {nonprofits.map(nonprofit => (
+          <Feature2Card key={nonprofit.id}
+          name={nonprofit.name}
+          description={nonprofit.description}
+          picture={nonprofit.picture}
+          website={nonprofit.website}
+        />
+        ))}
       </Box>
     </>
   );
